c0d85c3a0515e9d70197dd67a3f96465c2fae3d9
sebaw65 <sebaw65@gmail.com>
2023-09-19 23:16:51 +0200

    uploading new version of model managemnt

--- src/client/components/Scene/Scene.tsx
+++ src/client/components/Scene/Scene.tsx
@@ -1,25 +1,23 @@
 import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { CameraControls, Environment, Stage } from "@react-three/drei";
-import { Model } from "../Model";
+import { CameraControls, Environment } from "@react-three/drei";
+import { Models } from "../Models";
 import { StyledCanvas } from "./styled";
+import { Model } from "../Model";
 
 export const Scene = () => {
   return (
     <StyledCanvas
-      camera={{ position: [0, 15, 50], near: 0.01 }}
+      camera={{ position: [0, 15, 50], near: 0.1 }}
       frameloop="demand"
-      performance={{ min: 0.1 }}
       gl={{ antialias: false }}
     >
       <ambientLight intensity={0.9} />
-
       <Environment files={"/studio_2k.hdr"} />
+      <CameraControls />
 
       <Suspense fallback={null}>
         <Model />
       </Suspense>
-      <CameraControls />
     </StyledCanvas>
   );
 };
